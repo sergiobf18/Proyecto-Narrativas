7cfbd4cac53a08d291a3fd402ff1c6f59aa82db7
sergiobf18 <139904154+sergiobf18@users.noreply.github.com>
2024-05-24 11:27:37 -0500

    Delete Ejercicios directory
    eliminacion carpeta

--- Ejercicios/Inicio.php
+++
@@ -1,7 +0,0 @@
-<?php
-    echo "Ya desayunaron <br>";
-    print ("otro mensaje <br>");
-    print" tercer mensaje <br>";
-    print_r ("cuarto mensaje <br>"); //Arreglos
-    var_dump ("quinto mensaje");
-?>
